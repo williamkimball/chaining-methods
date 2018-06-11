336076e927391c22f0c20641d1e69836b5ae4539
William Kimball <wkkimball043@gmail.com>
2018-06-11 11:38:29 -0500

    finished chaining array methods

--- chaining.js
+++ chaining.js
@@ -1,2 +1,5 @@
 const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+// console.log(integers.sort((a, b) => b - a));
+// console.log(integers.sort((a, b) => b - a).filter(number => number <= 19));
+console.log(integers.sort((a, b) => b - a).filter(number => number <= 19).map(x => (x * 1.5)-1));
 
